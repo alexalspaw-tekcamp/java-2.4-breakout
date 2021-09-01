061c1e20b0f9b6d6f084cf050ad84480e5be645b
alexalspaw-tekcamp <aalspaw@teksystems.com>
2021-09-01 10:45:09 -0500

    added one signature to get started

--- src/com/teksystems/PodcastingApp.java
+++ src/com/teksystems/PodcastingApp.java
@@ -6,7 +6,7 @@ public class PodcastingApp {
 	   - appName (Apple Podcasts, Pocket Casts, Overcast, Google Podcasts, etc.)
        - playlist (of Podcasts)
        Methods :
-       - loadPlaylist (replace the existing playlist with a new one)
+       - loadPlaylist (replace the existing playlist with a new one, signature void loadPlaylist(List<Podcast newPlaylist)) 
        - addPodcast (add a new podcast to the playlist. order of the playlist should be maintained.)
 
        Below methods are suggestions if you make it this far
